e74b09e9bfc98dcf792f48266638bd0e697f7874
Reilly Brogan <reilly@reillybrogan.com>
2026-03-13 11:04:54 -0500

    legacy: Add vlc handler (#18)
    Co-authored-by: Evan Maddock <5157277+EbonJaeger@users.noreply.github.com>
    Co-authored-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/context.c
+++ src/context.c
@@ -86,6 +86,7 @@ static const UscHandler *usc_handlers[] = {
         &usc_handler_desktop_files,
         &usc_handler_gconf,
         &usc_handler_dconf,
+        &usc_handler_vlc,
 
         /* GTK immodules */
         &usc_handler_gtk2_immodules,
